e9e3e35b22ab7af0912ecabf77b25617916b5e52
dougsfelipe <dfcs@cin.ufpe.br>
2020-11-23 21:15:30 -0300

    Project done

---
+++ js/script.js
@@ -0,0 +1,25 @@
+function calcularIMC() {
+    var pesoInput = document.querySelector("#peso");
+    var alturaInput = document.querySelector("#altura");
+    var resultado = document.querySelector("#resposta");
+    var classificacaoOutput = document.querySelector("#classificacao");
+
+
+    var valorPeso = pesoInput.value;
+    var valorAltura = alturaInput.value;
+    var valorResultado = resultado.value;
+    var valorClassificacao = classificacaoOutput.value;
+
+    if (valorPeso <= 0) {
+        alert("Peso não pode ser nagativo");
+    } else if (valorAltura <= 0) {
+        alert("Peso não pode ser nagativo");
+    } else {
+        var IMC = valorPeso / (valorAltura * valorAltura);
+        resultado.textContent = IMC.toFixed(2);
+    }
+
+
+
+}
+
