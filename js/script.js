6703769640ee71e10742654d92879b8f873cd44a
dougsfelipe <dfcs@cin.ufpe.br>
2020-11-23 21:29:05 -0300

    desafio 4

--- js/script.js
+++ js/script.js
@@ -7,8 +7,9 @@ function calcularIMC() {
 
     var valorPeso = pesoInput.value;
     var valorAltura = alturaInput.value;
-    var valorResultado = resultado.value;
-    var valorClassificacao = classificacaoOutput.value;
+    var escala = null;
+
+
 
     if (valorPeso <= 0) {
         alert("Peso não pode ser nagativo");
@@ -17,6 +18,25 @@ function calcularIMC() {
     } else {
         var IMC = valorPeso / (valorAltura * valorAltura);
         resultado.textContent = IMC.toFixed(2);
+        
+
+
+        if(IMC < 14.5){
+            escala = "desnutrição";
+        }else if(IMC >= 14.5 && IMC < 20){
+            escala = "Peso abaixo do normal";
+        }else if(IMC >=20 && IMC < 24.9){
+            escala = "Peso normal";
+        }else if(IMC >=24.9 && IMC < 29.9){
+            escala = "Sobrepeso";
+        }else if(IMC >29.9 && IMC <39.9){
+            escala = "Obesidade";
+        }else{
+            escala = "Obesidade Morbida";
+        }
+
+        classificacaoOutput.textContent = escala;
+        
     }
 
 
